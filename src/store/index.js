50e12a718fb5dda91471cc0debd343dce3db197d
leobezr <le.bezr@gmail.com>
2020-11-30 20:42:36 -0300

    feat: attach knight module

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import Knights from "./modules/knights";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {},
+  modules: {
+     Knights
+  },
 });
