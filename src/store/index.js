e245761222cfc56cd04d012100bfacd288f5f922
leobezr <le.bezr@gmail.com>
2021-01-16 08:10:35 -0300

    feat: new module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import Knights from "./modules/knights";
+import Profile from "./modules/profile";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-     Knights
-  },
+   modules: {
+      Knights,
+      Profile
+   },
 });
