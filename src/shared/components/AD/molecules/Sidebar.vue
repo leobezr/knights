e3097ac9bf48bcb16cc744e651c0860b54f19837
leobezr <leonardo.ribeiro@finxi.com.br>
2021-01-16 16:46:23 -0300

    feat: sidebar links

--- src/shared/components/AD/molecules/Sidebar.vue
+++ src/shared/components/AD/molecules/Sidebar.vue
@@ -1,61 +1,54 @@
 <template>
-   <div id="sidebar">
-      <div class="userInfo">
-         <v-tooltip right>
-            <template v-slot:activator="{ on, attrs }">
-               <v-avatar v-bind="attrs" v-on="on">
-                  <img
-                     src="https://cdn.vuetifyjs.com/images/john.jpg"
-                     alt="John"
-                  />
-               </v-avatar>
-            </template>
-            <span>{{ characterName }}</span>
-         </v-tooltip>
-      </div>
-      <div class="menu-nav">
-         <ul>
-            <li>
-               <v-tooltip right>
-                  <template v-slot:activator="{ on, attrs }">
-                     <span class="circleBackground" v-bind="attrs" v-on="on">
-                        <v-icon color="white">mdi-face-profile</v-icon>
-                     </span>
-                  </template>
-                  <span>Profile</span>
-               </v-tooltip>
-            </li>
-            <li>
-               <v-tooltip right>
-                  <template v-slot:activator="{ on, attrs }">
-                     <span class="circleBackground" v-bind="attrs" v-on="on">
-                        <v-icon color="white">mdi-logout-variant</v-icon>
-                     </span>
-                  </template>
-                  <span>Signout</span>
-               </v-tooltip>
-            </li>
-         </ul>
-      </div>
-   </div>
+  <div id="sidebar">
+    <div class="userInfo">
+      <v-tooltip right>
+        <template v-slot:activator="{ on, attrs }">
+          <v-avatar v-bind="attrs" v-on="on">
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          </v-avatar>
+        </template>
+        <span>{{ characterName }}</span>
+      </v-tooltip>
+    </div>
+    <div class="menu-nav">
+      <SidebarLinks :links="sidebarLinks" />
+    </div>
+  </div>
 </template>
 
 <script>
 import "@/shared/scss/_sidebar.scss";
 import { mapGetters } from "vuex";
+import SidebarLinks from "@/shared/components/AD/atoms/SidebarLinks.vue";
 
 export default {
-   name: "Sidebar",
-   computed: {
-      ...mapGetters(["persona"]),
+  name: "Sidebar",
+  components: {
+    SidebarLinks,
+  },
+  computed: {
+    ...mapGetters(["persona"]),
 
-      characterName() {
-         if (this.persona) {
-            return this.persona.nickname;
-         } else {
-            return "Unknown user";
-         }
-      },
-   },
+    characterName() {
+      if (this.persona) {
+        return this.persona.nickname;
+      } else {
+        return "Unknown user";
+      }
+    },
+    sidebarLinks() {
+      return [
+        { icon: "mdi-face-profile", link: "Character", label: "Profile" },
+        { icon: "mdi-logout-variant", action: this.signout, label: "Signout" },
+      ];
+    },
+  },
+  methods: {
+    signout() {
+      localStorage.clear();
+
+      this.$router.push({ name: "KnightBuilder" }).catch(() => {});
+    },
+  },
 };
 </script>
