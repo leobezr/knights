2d90010236b4185b143556076ef88505439d2a0d
leobezr <leonardo.ribeiro@finxi.com.br>
2021-01-17 11:31:38 -0300

    feat: item sprite based modules

---
+++ src/shared/components/AD/atoms/ItemSprite.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="itemSprite">
+    <v-tooltip top color="indigo darken-4">
+      <template v-slot:activator="{ on, attrs }">
+        <figure v-bind="attrs" v-on="on" :tier="sprite.tier">
+          <img :src="spriteLink" :alt="sprite.name" />
+        </figure>
+      </template>
+      <span>
+        <div class="itemStats" tooltip>
+          <strong class="itemTitle">{{ sprite.name }}</strong>
+          <ul>
+            <li>
+              <strong>STR:</strong>
+              <span>{{ stats(sprite.attr.str) }}</span>
+            </li>
+            <li>
+              <strong>DEF:</strong>
+              <span>{{ stats(sprite.attr.armor) }}</span>
+            </li>
+            <li>
+              <strong>VIT:</strong>
+              <span>{{ stats(sprite.attr.vit) }}</span>
+            </li>
+            <li>
+              <strong>AGI:</strong>
+              <span>{{ stats(sprite.attr.agi) }}</span>
+            </li>
+            <li>
+              <strong>LUK:</strong>
+              <span>{{ stats(sprite.attr.luk) }}</span>
+            </li>
+          </ul>
+        </div>
+      </span>
+    </v-tooltip>
+  </div>
+</template>
+
+<script>
+import "@/shared/scss/_itemSprite.scss";
+
+export default {
+  name: "ItemSprite",
+  computed: {
+    spriteLink() {
+      const API_SERVER = process.env.VUE_APP_PUBLIC;
+
+      return API_SERVER + this.sprite.sprite;
+    },
+    stats() {
+      return (value) => {
+        return value ?? 0;
+      };
+    },
+  },
+  props: {
+    sprite: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/shared/components/AD/molecules/GearSprite.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="gearSprite">
+    <ul>
+      <li v-for="(sprite, index) in spriteList" :key="index">
+        <ItemSprite :sprite="sprite" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ItemSprite from "@/shared/components/AD/atoms/ItemSprite.vue";
+
+export default {
+  name: "GearSprite",
+  components: {
+    ItemSprite,
+  },
+  props: {
+    "sprite-list": {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
