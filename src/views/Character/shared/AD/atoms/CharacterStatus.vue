4329783864193eaaddb9e4ac1133fc42579656eb
leobezr <leonardo.ribeiro@finxi.com.br>
2021-01-17 11:31:20 -0300

    feat: character status system

--- src/views/Character/shared/AD/atoms/CharacterStatus.vue
+++ src/views/Character/shared/AD/atoms/CharacterStatus.vue
@@ -2,42 +2,42 @@
   <div id="characterStatus" class="mt-3">
     <strong>Character Status</strong>
 
-    <div class="table-grid mt-2">
+    <div class="table-grid mt-2" v-if="attribute">
       <div class="table-stats">
           <div class="line">
           <strong>HIT</strong>
-          <span>0</span>
+          <span>{{ attribute.hit }}</span>
         </div>
         <div class="line">
           <strong>ATK</strong>
-          <span>0</span>
+          <span>{{ attribute.atk }}</span>
         </div>
         <div class="line">
           <strong>DEF</strong>
-          <span>0</span>
+          <span>{{ attribute.def }}</span>
         </div>
         <div class="line">
           <strong>HP</strong>
-          <span>0 / 10</span>
+          <span>{{ `${attribute.hp.value} / ${attribute.hp.total}` }}</span>
         </div>
       </div>
 
       <div class="table-stats">
         <div class="line">
           <strong>STR</strong>
-          <span>0</span>
+          <span>{{ attribute.str }}</span>
         </div>
         <div class="line">
           <strong>VIT</strong>
-          <span>0</span>
+          <span>{{ attribute.vit }}</span>
         </div>
         <div class="line">
           <strong>AGI</strong>
-          <span>0</span>
+          <span>{{ attribute.agi }}</span>
         </div>
         <div class="line">
           <strong>LUK</strong>
-          <span>0</span>
+          <span>{{ attribute.luk }}</span>
         </div>
       </div>
     </div>
@@ -45,7 +45,23 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: "CharacterStatus",
+  data() {
+    return {
+      user: null  
+    }
+  },
+  computed: {
+    ...mapGetters(["persona"]),
+
+    attribute() {
+      return this.user?.attributes ?? {}
+    }
+  },
+  created() {
+    this.user = { ...this.persona };
+  }
 };
 </script>
