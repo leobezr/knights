3e23c45fca79cd62f9a966d1f743d5826d50bcba
leobezr <le.bezr@gmail.com>
2021-01-17 15:16:09 -0300

    feat: Gold placeholder

---
+++ src/views/Character/shared/AD/atoms/GoldPlaceholder.vue
@@ -0,0 +1,25 @@
+<template>
+   <div id="goldPlaceholder">
+      <v-text-field label="Gold" color="amber" readonly :value="gold" prepend-inner-icon="mdi-gold" dark dense hide-details outlined>
+         <template v-slot:prepend-inner>
+            <v-icon color="amber" left>mdi-gold</v-icon>
+         </template>
+      </v-text-field>
+   </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+   name: "GoldPlaceholder",
+   computed: {
+      ...mapGetters(["persona"]),
+
+      gold() {
+         if (this.persona) {
+            return this.persona.gold ?? 0
+         }
+      }
+   }
+}
+</script>
