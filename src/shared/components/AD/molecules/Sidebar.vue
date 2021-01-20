631e78854f088c11cb81138164e5ab7eb710bdff
leobezr <le.bezr@gmail.com>
2021-01-20 16:07:18 -0300

    refactor: equips

--- src/shared/components/AD/molecules/Sidebar.vue
+++ src/shared/components/AD/molecules/Sidebar.vue
@@ -1,19 +1,22 @@
 <template>
-  <div id="sidebar">
-    <div class="userInfo">
-      <v-tooltip right>
-        <template v-slot:activator="{ on, attrs }">
-          <v-avatar v-bind="attrs" v-on="on">
-            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
-          </v-avatar>
-        </template>
-        <span>{{ characterName }}</span>
-      </v-tooltip>
-    </div>
-    <div class="menu-nav">
-      <SidebarLinks :links="sidebarLinks" />
-    </div>
-  </div>
+   <div id="sidebar">
+      <div class="userInfo">
+         <v-tooltip right>
+            <template v-slot:activator="{ on, attrs }">
+               <v-avatar v-bind="attrs" v-on="on">
+                  <img
+                     src="https://cdn.vuetifyjs.com/images/john.jpg"
+                     alt="John"
+                  />
+               </v-avatar>
+            </template>
+            <span>{{ characterName }}</span>
+         </v-tooltip>
+      </div>
+      <div class="menu-nav">
+         <SidebarLinks :links="sidebarLinks" />
+      </div>
+   </div>
 </template>
 
 <script>
@@ -22,33 +25,57 @@ import { mapGetters } from "vuex";
 import SidebarLinks from "@/shared/components/AD/atoms/SidebarLinks.vue";
 
 export default {
-  name: "Sidebar",
-  components: {
-    SidebarLinks,
-  },
-  computed: {
-    ...mapGetters(["persona"]),
+   name: "Sidebar",
+   data() {
+      return {
+         sessionId: null,
+      };
+   },
+   components: {
+      SidebarLinks,
+   },
+   computed: {
+      ...mapGetters(["persona"]),
 
-    characterName() {
-      if (this.persona) {
-        return this.persona.nickname;
-      } else {
-        return "Unknown user";
-      }
-    },
-    sidebarLinks() {
-      return [
-        { icon: "mdi-face-profile", link: "Character", label: "Profile" },
-        { icon: "mdi-logout-variant", action: this.signout, label: "Signout" },
-      ];
-    },
-  },
-  methods: {
-    signout() {
-      localStorage.clear();
+      characterName() {
+         if (this.persona) {
+            return this.persona.nickname;
+         } else {
+            return "Unknown user";
+         }
+      },
+      sidebarLinks() {
+         return [
+            {
+               icon: "mdi-face-profile",
+               link: "Character",
+               params: {
+                  id: this.sessionId,
+               },
+               label: "Profile",
+            },
+            { icon: "mdi-treasure-chest", link: "Store", label: "Store" },
+            {
+               icon: "mdi-logout-variant",
+               action: this.signout,
+               label: "Signout",
+            },
+         ];
+      },
+   },
+   methods: {
+      signout() {
+         localStorage.clear();
 
-      this.$router.push({ name: "KnightBuilder" }).catch(() => {});
-    },
-  },
+         this.$router.push({ name: "KnightBuilder" }).catch(() => {});
+      },
+   },
+   watch: {
+      $route: {
+         handler() {
+            this.sessionId = localStorage.sessionId;
+         },
+      },
+   },
 };
 </script>
