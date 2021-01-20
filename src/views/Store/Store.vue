020d9ae044f197b134585790278e0f78b391540b
leobezr <le.bezr@gmail.com>
2021-01-20 16:05:25 -0300

    feat: store page

---
+++ src/views/Store/Store.vue
@@ -0,0 +1,37 @@
+<template>
+   <div id="character">
+      <div class="landscape" :style="backgroundImage">
+         <div class="store">
+            Store
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import bgImage from "@/shared/img/store-background.jpg";
+import "@/views/Character/shared/scss/_character.scss";
+
+export default {
+   name: "Store",
+   computed: {
+      backgroundImage() {
+         return `background-image: url(${bgImage});`;
+      },
+   },
+   components: {
+
+   },
+   methods: {
+      ...mapActions(["me"]),
+
+      init() {
+         this.me().finally(() => (this.fetchingProfileData = false));
+      },
+   },
+   created() {
+      this.init();
+   },
+};
+</script>
