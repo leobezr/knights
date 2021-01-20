d00d84eccfa6b94d74e8d84c3f9fef98d1703d4f
leobezr <le.bezr@gmail.com>
2021-01-20 16:04:20 -0300

    chore: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,53 @@
+const path = require("path");
+const webpack = require("webpack");
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.s(c|a)ss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            // Requires sass-loader@^7.0.0
+   module: {
+      rules: [
+         {
+            test: /\.scss$/,
+            use: ["vue-style-loader", "css-loader", "sass-loader"]
+         },
+         {
+            test: /\.csv$/,
+            loader: 'csv-loader',
             options: {
-              implementation: require('sass'),
-              indentedSyntax: true // optional
-            },
-            // Requires sass-loader@^8.0.0
+               dynamicTyping: true,
+               header: true,
+               skipEmptyLines: true
+            }
+         },
+         {
+            test: /\.(csv|xlsx|xls)$/,
+            loader: 'file-loader',
             options: {
-              implementation: require('sass'),
-              sassOptions: {
-                indentedSyntax: true // optional
-              },
-            },
-          },
-        ],
-      },
-    ],
-  }
-}
+               name: `files/[name].[ext]`
+            }
+         },
+         {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/,
+         },
+      ],
+   },
+   output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name]-[hash].js',
+      chunkFilename: '[id]-[chunkhash].js',
+   },
+   plugins: [
+      new WebpackAssetsManifest({
+         publicPath: process.env.VUE_APP_FRONTEND_ROOT_URL,
+      }),
+      new webpack.DefinePlugin({
+         "API_URL": process.env.VUE_APP_FRONTEND_ROOT_URL
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+         name: "vendor",
+         filename: "vendor.js",
+         minChunks: ({ resource }) => /node_modules/.test(resource)
+      }),
+   ],
+};
