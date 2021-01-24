8bc419b5f087421340ffe10f3cde08180b7eeb99
leobezr <le.bezr@gmail.com>
2021-01-23 21:05:27 -0300

    feat: hunt

---
+++ src/views/Hunt/shared/maps/index.js
@@ -0,0 +1,21 @@
+import PoringSprite from "@/views/Hunt/shared/img/monsters/porings/poring.gif";
+import PoringBackgroundSprite from "@/views/Hunt/shared/img/monsters/porings/thumb-background.png";
+
+import ZairoSprite from "@/views/Hunt/shared/img/monsters/goblins/zairo.gif";
+
+export default [
+   {
+      name: "Poring",
+      sprite: PoringSprite,
+      level: 1,
+      bg: PoringBackgroundSprite,
+      scale: 1.5,
+   },
+   {
+      name: "Zairo",
+      sprite: ZairoSprite,
+      level: 30,
+      bg: PoringBackgroundSprite,
+      scale: 1,
+   },
+]
