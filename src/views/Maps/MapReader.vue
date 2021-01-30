f6d1c8c3003508514a527225ad536901824f13a6
leobezr <le.bezr@gmail.com>
2021-01-30 16:43:28 -0300

    feat: new sprite and speed logic

--- src/views/Maps/MapReader.vue
+++ src/views/Maps/MapReader.vue
@@ -6,20 +6,26 @@
          :style="mapBackground"
          :class="canvasState"
          @mousedown="attack"
+         v-if="!shouldUpdate"
          ref="Canvas"
       />
       <div class="battleStats" v-if="canvasState">
          <div class="winner" v-if="canvasState == 'winner'">
-            <span>WINNER</span>
-            <v-btn
-               color="amber"
-               @click="backToHuntLobby('wone')"
-               :loading="levelLoading"
-               >Get reward</v-btn
-            >
+            <span>COMPLETE</span>
+            <div class="controller">
+               <v-btn @click="goAgain" class="mr-2" color="primary"
+                  >Go again</v-btn
+               >
+               <v-btn
+                  class="pink accent-3 white--text"
+                  @click="backToHuntLobby('wone')"
+                  :loading="levelLoading"
+                  >Get reward</v-btn
+               >
+            </div>
          </div>
          <div class="loser" v-else>
-            <span>LOSER</span>
+            <span>FAILED</span>
             <v-btn
                class="pink accent-3 white--text"
                @click="backToHuntLobby"
@@ -35,24 +41,24 @@
 import mapConfig from "@/views/Maps/monsters/map-config.js";
 import CanvasDrawer from "@/views/Maps/config/canvas-drawer.js";
 import "@/views/Maps/shared/scss/_mapReader.scss";
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
    name: "MapReader",
    data() {
       return {
          battle: null,
-         characterStats: {
-            life: 10000,
-            dmg: 10,
-         },
+         characterStats: {},
          enemy: null,
          mapName: null,
          map: null,
          levelLoading: false,
+         shouldUpdate: false,
       };
    },
    computed: {
+      ...mapGetters(["persona"]),
+
       maxWindowHeight() {
          return Math.round(window.innerHeight - 80);
       },
@@ -72,10 +78,58 @@ export default {
          }
          return false;
       },
+      computedStats() {
+         let stats = {};
+
+         for (let attr in this.characterStats.attributes) {
+            stats[attr] = this.characterStats.attributes[attr];
+         }
+         for (let attr in this.characterStats.modifier) {
+            stats[attr] += this.characterStats.modifier[attr];
+         }
+         return stats;
+      },
+      battleStats() {
+         if (this.characterStats) {
+            return {
+               damage: this.characterStats.attributes.hit,
+               hp: this.characterStats.attributes.hp,
+               def: this.characterStats.attributes.def,
+               agi: this.computedStats.agi,
+            };
+         } else {
+            return false;
+         }
+      },
    },
    methods: {
-      ...mapActions(["getReward"]),
+      ...mapActions(["getReward", "me"]),
 
+      goAgain() {
+         this.battle = null;
+         this.shouldUpdate = true;
+
+         setTimeout(() => {
+            this.shouldUpdate = false;
+         }, 10);
+         setTimeout(() => {
+            this.setMap();
+            this.setKeyFunction();
+         }, 15);
+      },
+      init() {
+         if (this.persona) {
+            this.createInstance();
+         } else {
+            this.me();
+         }
+      },
+      createInstance() {
+         if (!this.map) {
+            this.setMap();
+            this.setKeyFunction();
+         }
+      },
       setMap() {
          const MAP = this.$route.params.hunt;
 
@@ -85,7 +139,7 @@ export default {
                win: this.winMatch,
                lose: this.loseMatch,
                $canvas: this.$refs.Canvas,
-               playerStats: this.characterStats,
+               playerStats: this.battleStats,
                monster: mapConfig.enemies[MAP],
             });
             this.enemy = mapConfig.enemies[MAP].id;
@@ -125,9 +179,19 @@ export default {
          this.battle = false;
       },
    },
+   watch: {
+      persona: {
+         handler() {
+            this.characterStats = this.persona;
+            this.createInstance();
+         },
+      },
+   },
+   created() {
+      this.characterStats = this.persona;
+   },
    mounted() {
-      this.setMap();
-      this.setKeyFunction();
+      this.init();
    },
    beforeDestroy() {
       window.removeEventListener("keydown", this.arrowWalk, false);
