0ec47420462df4979bb4c46f08aa1c04a0cb191e
leobezr <le.bezr@gmail.com>
2021-02-14 23:05:11 -0300

    feat: Hunt and User store managers

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,17 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import Knights from "./modules/knights";
-import Profile from "./modules/profile";
 import Items from "./modules/items";
 import Hunt from "./modules/hunt";
+import User from "./modules/user";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
    modules: {
       Knights,
-      Profile,
       Items,
-      Hunt
+      Hunt,
+      User
    },
 });
