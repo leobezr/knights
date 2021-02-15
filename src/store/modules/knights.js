200d59310e5a7c579f398c72320830c897d943f4
leobezr <le.bezr@gmail.com>
2021-02-14 23:06:54 -0300

    feat: update account structure

--- src/store/modules/knights.js
+++ src/store/modules/knights.js
@@ -1,69 +1,77 @@
 import { knights } from "@/service/endpoints";
-import Session from "@/session/localSession";
+import Session from "@/session/localSession.js";
+import Router from "@/router/index.js";
 
 export default {
    actions: {
-      async createKnight({ }, user) {
-         try {
-            let req = await knights.create(user);
-
-            Session.startNewSession(req.data.id);
-            return req.data;
-         } catch (err) {
-            throw new Error(err);
-         }
-
-      },
-      async getKnight({ }, id) {
-         return await knights.find(id);
-      },
-      async equipItem({ dispatch }, item) {
-         await knights.equip({
-            id: localStorage.sessionId,
-            equip: item
-         })
-
-         dispatch("me");
-      },
-      async unequipItem({ dispatch }, props) {
-         await knights.unequip({
-            id: localStorage.sessionId,
-            slot: props.slot
-         })
-
-         dispatch("me");
-      },
-      async discardInventoryItem({ dispatch }, item) {
-         await knights.discardItem({
-            id: localStorage.sessionId,
-            equip: item
-         })
-
-         dispatch("me");
-      },
       async addPoint({ dispatch }, attr) {
-         await knights.addPoint({
-            id: localStorage.sessionId,
-            attr
-         })
+         await knights.addPoint({ attr })
 
          await dispatch("me");
          return;
+      },
+      async createCharacter({ dispatch }, user) {
+         try {
+            await knights.add(user);
+            await dispatch("getUserData");
+            return;
+         } catch (err) {
+            throw Error(err);
+         }
+      },
+      async discardInventoryItem({ dispatch }, item) {
+         await knights.discardItem({ equip: item })
+
+         dispatch("me");
+      },
+      async equipItem({ dispatch }, item) {
+         await knights.equip({ equip: item })
+
+         dispatch("me");
       },
       async getStoredReward({ dispatch }) {
          await knights.getReward()
 
          await dispatch("me");
          return;
-      }
+      },
+      async getKnight({ }, id) {
+         return await knights.find(id);
+      },
+      async me({ commit, dispatch }) {
+         try {
+            if (localStorage.charToken) {
+               let persona = await knights.login();
+
+               commit("updatePersona", persona.data);
+               return persona.data;
+            } else if (localStorage.userToken) {
+               return await dispatch("getUserData");
+            } else {
+               throw "Session ID is invalid";
+            }
+         } catch (err) {
+            Session.clear();
+
+            Router.push({ name: "Login" }).catch(e => { });
+            throw err;
+         }
+      },
+      async unequipItem({ dispatch }, props) {
+         await knights.unequip({ slot: props.slot })
+
+         dispatch("me");
+      },
    },
    mutations: {
-
+      updatePersona(state, data) {
+         state.persona = data;
+      }
    },
    state: {
-
+      persona: null
    },
    getters: {
-
-   }
+      persona: state => state.persona
+   },
 }
