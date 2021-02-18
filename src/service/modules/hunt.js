4d5c6ec4a9ed91ceae47264b033acc4db64ee209
leobezr <le.bezr@gmail.com>
2021-02-18 14:41:46 -0300

    feat: new endpoints

--- src/service/modules/hunt.js
+++ src/service/modules/hunt.js
@@ -1,21 +1,33 @@
 import Api from "@/service/api";
 
-const prefix = "hunt/reward/";
-
-export default {
-   reward: getRewards,
-   bossReward
-}
+const prefix = "hunt/";
 
 function getRewards(payload) {
    return Api.POST({
-      url: prefix,
+      url: prefix + "reward/",
       body: payload
    })
 }
 function bossReward(payload) {
    return Api.POST({
-      url: prefix + "boss/",
+      url: prefix + "reward/boss/",
       body: payload
    })
+}
+function huntLevels() {
+   return Api.GET({
+      url: prefix + "levels",
+   })
+}
+function bosses() {
+   return Api.GET({
+      url: prefix + "bosses",
+   })
+}
+
+export default {
+   reward: getRewards,
+   bossReward,
+   huntLevels,
+   bosses
 }
--- src/store/modules/hunt.js
+++ src/store/modules/hunt.js
@@ -12,14 +12,42 @@ export default {
 
          return await dispatch("me");
       },
+      async getHuntLevels({ commit }) {
+         let huntLevels = await hunt.huntLevels();
+         commit("updateHuntLevels", huntLevels.data);
+      },
+      async getBossLevels({ commit }) {
+         let bosses = await hunt.bosses();
+         commit("updateBossLevels", bosses.data);
+      }
    },
    mutations: {
       updateItemGallery(state, itemList) {
          state.gallery = itemList;
-      }
+      },
+      updateHuntLevels(state, hunts) {
+         state.huntLevels = {};
+
+         for (let key in hunts) {
+            if (typeof hunts[key] == "object") {
+               state.huntLevels[key] = hunts[key];
+            }
+         }
+      },
+      updateBossLevels(state, hunts) {
+         state.bossLevel = {};
+
+         for (let key in hunts) {
+            if (typeof hunts[key] == "object") {
+               state.bossLevels[key] = hunts[key];
+            }
+         }
+      },
    },
    state: {
-      gallery: []
+      gallery: [],
+      huntLevels: {},
+      bossLevels: {}
    },
    getters: {
 
